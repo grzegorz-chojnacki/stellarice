531d11cd941eeb9993f63fde71e64affc53b1051
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2022-07-22 21:55:54 +0200

    Include references in sections

--- main.js
+++ main.js
@@ -11,19 +11,22 @@ const empire = {
 }
 
 // Section templates
-//   name     - the name displayed at the top of the section
-//   items    - items associated with this section
-//   details  - additional details about this section
-//   template - HTML tamplate dependent on a given item
+//   name      - the name displayed at the top of the section
+//   items     - items associated with this section
+//   template  - HTML tamplate dependent on a given item
+//   refernces - Data and HTML node references used for updating the view
+//   details   - additional details about this section
 const sections = [
   {
     name: 'pop',
     items: pop,
     template: sectionTemplate('radio'),
+    references: {},
   },
   {
     name: 'traits',
     items: traits,
+    references: {},
     details: () => `
       Available traits:
         <span class="trait-point">${5 - empire.traits.length}</span><br>
@@ -37,30 +40,31 @@ const sections = [
     name: 'origin',
     items: origin,
     template: sectionTemplate('radio'),
+    references: {},
   },
   {
     name: 'ethics',
     items: ethics,
     template: sectionTemplate('checkbox'),
+    references: {},
   },
   {
     name: 'authority',
     items: authority,
     template: sectionTemplate('radio'),
+    references: {},
   },
   {
     name: 'civics',
     items: civics,
     template: sectionTemplate('checkbox'),
+    references: {},
     details: () => `Available civics: ${2 - empire.civics.length}`,
   },
 ]
 
-// List references to various HTML nodes grouped by sections
-const references = []
-
 const updateView = () => {
-  references.forEach(({ summary, header, details, inputs }) => {
+  sections.references.forEach(({ summary, header, details, inputs }) => {
     updateSummary(sortSummary(summary))
     updateHeader(header)
 
@@ -82,42 +86,44 @@ const renderView = () => {
   summary.appendChild(document.createElement('h2')).append('Empire summary')
   const table = summary.appendChild(document.createElement('table'))
 
-  sections.forEach(({ name, items, template, details = () => '' }) => {
-    const row = table.insertRow()
-    row.appendChild(document.createElement('th')).append(capitalize(name))
+  sections.references = sections.map(
+    ({ name, items, template, details = () => '' }) => {
+      const row = table.insertRow()
+      row.appendChild(document.createElement('th')).append(capitalize(name))
 
-    const section = options.appendChild(document.createElement('section'))
-    const header = section.appendChild(document.createElement('h2'))
-    header.innerHTML = capitalize(name)
+      const section = options.appendChild(document.createElement('section'))
+      const header = section.appendChild(document.createElement('h2'))
+      header.innerHTML = capitalize(name)
 
-    const handle = section.appendChild(document.createElement('div'))
-    const inputList = section.appendChild(document.createElement('div'))
-    inputList.classList.add('input-list')
+      const handle = section.appendChild(document.createElement('div'))
+      const inputList = section.appendChild(document.createElement('div'))
+      inputList.classList.add('input-list')
 
-    const inputs = items.map(item => {
-      const element = inputList.appendChild(htmlToElement(template(item)))
-      const handle = element.getElementsByTagName('input')[0]
-      const tooltip = element.getElementsByClassName('tooltip')[0]
+      const inputs = items.map(item => {
+        const element = inputList.appendChild(htmlToElement(template(item)))
+        const handle = element.getElementsByTagName('input')[0]
+        const tooltip = element.getElementsByClassName('tooltip')[0]
 
-      element.classList.add(getColor(item))
+        element.classList.add(getColor(item))
 
-      const rules = generateRules(tooltip, item.rule)
+        const rules = generateRules(tooltip, item.rule)
 
-      handle.onclick = () => {
-        toggleIncluded(empire[name], item)
-        updateView()
+        handle.onclick = () => {
+          toggleIncluded(empire[name], item)
+          updateView()
+        }
+
+        return { item, handle, rules }
+      })
+
+      return {
+        summary: { handle: row.insertCell(), items: empire[name], name },
+        header: { handle: header, items },
+        details: { handle, refresh: details },
+        inputs,
       }
-
-      return { item, handle, rules }
-    })
-
-    references.push({
-      summary: { handle: row.insertCell(), items: empire[name], name },
-      header: { handle: header, items },
-      details: { handle, refresh: details },
-      inputs,
-    })
-  })
+    }
+  )
 }
 
 // Initialize the view
